a51290edc3ada324173724e1a99c56a8c975fa9e
Nick Rowlands <89347+rowlando@users.noreply.github.com>
2023-08-29 16:50:22 +0100

    Fix TS with root

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root') as Element);
 root.render(
   <React.StrictMode>
     <App />
